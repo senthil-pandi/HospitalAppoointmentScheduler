1bdc90ed637afd0eb21a081223e0d85b4f808cbf
Senthil Pandi <80332839+senthil-pandi@users.noreply.github.com>
2022-02-17 08:44:52 +0530

    Add project files.

---
+++ HospitalAppoointmentScheduler/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using HospitalAppointmentScheduler.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalAppointmentScheduler.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospitalAppoointmentScheduler/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalAppointmentScheduler.Models
+{
+    public class ApplicationDbContext : IdentityDbContext //To use identity[log in]
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
